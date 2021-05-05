d602f9f67bc52683e1790be3bf8bd7fa02c7a71b
Archana-arch124 <archgowda124@gmail.com>
2021-05-05 13:35:14 +0530

    [Archana]Added:Updated files in bookstore

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,10 @@
 
-import { createStore,combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import getBooks  from '../Redux/Reducers/reducers'
+import getBooks from '../Redux/Reducers/reducers'
 
 const reducers = combineReducers({
-    Books:getBooks
+    Books: getBooks
 })
 
 const store = createStore(reducers, (applyMiddleware(thunk)));
